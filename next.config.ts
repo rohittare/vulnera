f534cfe84706dfc16cdac69bc1ec6ce8d756ba26
ritrot <tarerohit.r@gmail.com>
2025-09-27 15:41:55 +0530

    build error fixed

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/home",
-        permanent: true, // set to false if it's temporary
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { redirect } from "next/navigation";
+const page = () => {
+    redirect("/home");
+}
+
+export default page
